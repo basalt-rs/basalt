305ce7e053b6291b8612b529f12a8fe93b072166
erik <erikclaros.bus@gmail.com>
2025-03-21 00:11:08 -0500

    Added dialog for settings and tabs for editor

--- client/components/UserMenu.tsx
+++ client/components/UserMenu.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useTheme } from 'next-themes';
+import { useState } from 'react';
 import { Button } from './ui/button';
 import { User, Sun, Moon, SunMoon, LogOut } from 'lucide-react';
 import {
@@ -13,55 +14,74 @@ import {
     DropdownMenuSubTrigger,
     DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog';
 import { useRouter } from 'next/navigation';
 import { useSetAtom } from 'jotai';
 import { tokenAtom } from '@/lib/auth';
 import { RESET } from 'jotai/utils';
+import { SettingsPanel } from './Settings';
 
 export default function UserMenu() {
     const { setTheme } = useTheme();
     const setToken = useSetAtom(tokenAtom);
     const router = useRouter();
-
+    const [open, setOpen] = useState(false);
     const logout = () => {
         setToken(RESET);
         router.replace('/');
     };
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button size="icon" variant="outline">
-                    <User />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuPortal>
-                <DropdownMenuContent>
-                    <DropdownMenuSub>
-                        <DropdownMenuSubTrigger>
-                            <SunMoon />
-                            Theme
-                        </DropdownMenuSubTrigger>
-                        <DropdownMenuPortal>
-                            <DropdownMenuSubContent>
-                                <DropdownMenuItem onClick={() => setTheme('light')}>
-                                    <Sun className="pr-0.5" />
-                                    Light
-                                </DropdownMenuItem>
-                                <DropdownMenuItem onClick={() => setTheme('dark')}>
-                                    <Moon className="pr-0.5" />
-                                    Dark
-                                </DropdownMenuItem>
-                            </DropdownMenuSubContent>
-                        </DropdownMenuPortal>
-                    </DropdownMenuSub>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem onClick={logout}>
-                        <LogOut />
-                        Log Out
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenuPortal>
-        </DropdownMenu>
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <User />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuPortal>
+                    <DropdownMenuContent>
+                        <DropdownMenuSub>
+                            <DropdownMenuSubTrigger>
+                                <SunMoon />
+                                Theme
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem onClick={() => setTheme('light')}>
+                                        <Sun className="pr-0.5" />
+                                        Light
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => setTheme('dark')}>
+                                        <Moon className="pr-0.5" />
+                                        Dark
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem onClick={() => setOpen(true)}>
+                            Editor Configurations
+                        </DropdownMenuItem>
+
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onClick={logout}>
+                            <LogOut />
+                            Log Out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenuPortal>
+            </DropdownMenu>
+
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent className="justify-center">
+                    <DialogHeader>
+                        <DialogTitle className="flex justify-center">Settings</DialogTitle>
+                    </DialogHeader>
+                    <SettingsPanel />
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
