afc7488180f6dbefca3e9b3a72dc725a2b763e34
erik <erikclaros.bus@gmail.com>
2025-04-02 21:38:39 -0500

    Added compeititor-state file

---
+++ client/lib/competitor-state.ts
@@ -0,0 +1,34 @@
+import { atomWithStorage } from 'jotai/utils';
+
+export interface EditorSettings {
+    theme: string;
+    autoIndent: boolean;
+    showLineNumbers: boolean;
+    basicAutocompletion: boolean;
+    liveAutocompletion: boolean;
+    highlightActiveLine: boolean;
+    relativeLine: boolean;
+    showIndentGuides: boolean;
+    fontSize: number;
+    softTabs: number;
+    keybind: string;
+    cursorStyle: string;
+    foldStyle: 'manual' | 'markbegin' | 'markbeginend' | undefined;
+}
+
+// Default Editor Configurations
+export const editorSettingsAtom = atomWithStorage<EditorSettings>('editor-settings', {
+    theme: 'monokai',
+    autoIndent: true,
+    showLineNumbers: true,
+    basicAutocompletion: true,
+    liveAutocompletion: true,
+    highlightActiveLine: false,
+    relativeLine: false,
+    showIndentGuides: false,
+    fontSize: 12,
+    softTabs: 4,
+    keybind: 'ace',
+    cursorStyle: 'ace',
+    foldStyle: 'manual',
+});
