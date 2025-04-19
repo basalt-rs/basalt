01144ed02899cc89706f752630540ab25a344476
funnyboy-roks <funnyboyroks@gmail.com>
2025-04-18 20:23:26 -0500

    start submission history

--- client/app/host/TeamInfo.tsx
+++ client/app/host/TeamInfo.tsx
@@ -8,35 +8,19 @@ export default function TeamInfo() {
     return (
         selectedTeam !== null && (
             <div className="flex h-full w-full flex-col p-4">
-                <div className="w-full">
-                    <span className="flex items-center justify-between text-2xl font-bold">
-                        {selectedTeam.name}
-                        {selectedTeam.status ? (
-                            <p className="text-green-500">Connected</p>
-                        ) : (
-                            <p className="text-gray-300 dark:text-gray-500">Disconnected</p>
-                        )}
-                    </span>
-                </div>
-                <div className="flex h-full w-full flex-col gap-4 text-lg">
-                    <span className="flex justify-between align-middle">
+                <div className="flex items-center justify-between font-bold">
+                    <div className="flex flex-col">
+                        <p className="text-2xl">{selectedTeam.name}</p>
                         <p>
                             <strong>Points: </strong>
                             {selectedTeam.points}
                         </p>
-                        <Button
-                            variant="outline"
-                            onClick={() => {
-                                toast({
-                                    title: 'Coming Soon',
-                                    description: 'This feature is coming soon!',
-                                    variant: 'destructive',
-                                });
-                            }}
-                        >
-                            Submission History
-                        </Button>
-                    </span>
+                    </div>
+                    {selectedTeam.status ? (
+                        <p className="text-green-500 text-2xl">Connected</p>
+                    ) : (
+                        <p className="text-gray-300 dark:text-gray-500 text-2xl">Disconnected</p>
+                    )}
                 </div>
             </div>
         )
