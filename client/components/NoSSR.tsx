7a0d07765a30613cf7bb4232ea3c4abc10d52851
funnyboy-roks <funnyboyroks@gmail.com>
2025-04-20 22:08:40 -0500

    prettier

--- client/components/NoSSR.tsx
+++ client/components/NoSSR.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren, useEffect, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from 'react';
 
 export const NoSSR = ({ children }: PropsWithChildren) => {
-    const [canRender, setCanRender] = useState(false)
+    const [canRender, setCanRender] = useState(false);
     useEffect(() => setCanRender(true), [setCanRender]);
     return canRender ? children : <span />;
-}
+};
