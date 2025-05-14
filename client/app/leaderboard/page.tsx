62e3d5c2dc9d34405ad842ac9f2f471389d6a8e5
Austin Adams <asadams110102@gmail.com>
2025-05-13 19:13:26 -0500

    Leaderboard redesign with back button and gamecode display

--- client/app/leaderboard/page.tsx
+++ client/app/leaderboard/page.tsx
@@ -1,7 +1,52 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Leaderboard from '@/components/Leaderboard';
+import { useAtom } from 'jotai';
+import { ipOrGameCodeAtom } from '@/lib/services/api';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
-    return <Leaderboard />;
+    const router = useRouter();
+
+    const GameCodeModal = () => {
+        const [openModal, setOpenModal] = useState(false);
+        const [gameCode] = useAtom(ipOrGameCodeAtom);
+
+        return (
+            <>
+                <div>
+                    <Button variant="outline" onClick={() => setOpenModal((prev) => !prev)}>
+                        Game Code
+                    </Button>
+                </div>
+                {openModal && (
+                    <Dialog open={openModal} onOpenChange={() => setOpenModal(false)}>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Game Code</DialogTitle>
+                            </DialogHeader>
+                            {gameCode}
+                        </DialogContent>
+                    </Dialog>
+                )}
+            </>
+        );
+    };
+
+    return (
+        <div className="flex h-screen flex-col">
+            <div className="m-2">
+                <Button variant="ghost" onClick={() => router.push('/')}>
+                    <ArrowLeft /> Back
+                </Button>
+            </div>
+            <Leaderboard />
+            <div className="mx-2 mb-2 mt-auto flex justify-end">
+                <GameCodeModal />
+            </div>
+        </div>
+    );
 }
